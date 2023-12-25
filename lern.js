8904863fe6da423a69736b6dba4eb665a45f7094
Abdullah <abdullahkhaled22poody@gmail.com>
2023-12-25 22:49:18 +0200

    Add files via upload

---
+++ lern.js
@@ -0,0 +1,53 @@
+let titel=document.querySelector('.title')
+let turn='x';
+let squares=[];
+function end(num1,num2,num3){
+    titel.innerHTML=`${squares[num1]} winer`
+    document.getElementById('item'+num1).style.background='black'
+    document.getElementById('item'+num2).style.background='black'
+    document.getElementById('item'+num3).style.background='black'
+    setInterval(function(){titel.innerHTML+='.'},1000)
+    setTimeout(function(){location.reload()},4000)
+}
+function winner(){
+for(let i=1 ;i<10 ; i++){
+    squares[i]=document.getElementById('item'+i).innerHTML;
+}
+if(squares[1]==squares[2]&&squares[2]==squares[3]&&squares[2]!=''){
+    end(1,2,3)
+    
+}else if(squares[4]==squares[5]&&squares[5]==squares[6]&&squares[5]!=''){
+    end(4,5,6)
+    
+}else if(squares[7]==squares[8]&&squares[8]==squares[9]&&squares[8]!=''){
+    end(7,8,9)
+}
+else if(squares[1]==squares[4]&&squares[4]==squares[7]&&squares[1]!=''){
+    end(1,4,7)
+}else if(squares[2]==squares[5]&&squares[5]==squares[8]&&squares[2]!=''){
+    end(2,5,8)
+}else if(squares[3]==squares[6]&&squares[6]==squares[9]&&squares[6]!=''){
+    end(3,6,9)
+}
+else if(squares[1]==squares[5]&&squares[5]==squares[9]&&squares[9]!=''){
+    end(1,5,9)
+}
+else if(squares[3]==squares[5]&&squares[5]==squares[7]&&squares[7]!=''){
+    end(3,5,7)
+}
+}
+function game(id){
+    let elemnet =document.getElementById(id)
+if(turn === 'x'&& elemnet.innerHTML==''){
+elemnet.innerHTML='x'
+turn='o'
+titel.innerHTML='o'
+}
+else if(turn ==='o'&&elemnet.innerHTML==''){
+    elemnet.innerHTML='o'
+    turn='x'
+    titel.innerHTML='x';
+
+}
+winner()
+}
